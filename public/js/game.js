a38de457507cb96cfe15685ba1c8247544bc2fcf
Ricardo de Cillo <cillo.ricardo@gmail.com>
2013-03-23 02:20:37 -0300

    fast click for ios

--- public/js/game.js
+++ public/js/game.js
@@ -197,21 +197,26 @@ socket.on('start', function(data) {
   
 // });
 
-(function($) {
-  var IS_IOS = /iphone|ipad/i.test(navigator.userAgent);
-  $.fn.nodoubletapzoom = function() {
-    if (IS_IOS)
-      $(this).bind('touchstart', function preventZoom(e) {
-        var t2 = e.timeStamp
-          , t1 = $(this).data('lastTouch') || t2
-          , dt = t2 - t1
-          , fingers = e.originalEvent.touches.length;
-        $(this).data('lastTouch', t2);
-        if (!dt || dt > 500 || fingers > 1) return; // not double-tap
+// (function($) {
+//   var IS_IOS = /iphone|ipad/i.test(navigator.userAgent);
+//   $.fn.nodoubletapzoom = function() {
+//     if (IS_IOS)
+//       $(this).bind('touchstart', function preventZoom(e) {
+//         var t2 = e.timeStamp
+//           , t1 = $(this).data('lastTouch') || t2
+//           , dt = t2 - t1
+//           , fingers = e.originalEvent.touches.length;
+//         $(this).data('lastTouch', t2);
+//         if (!dt || dt > 500 || fingers > 1) return; // not double-tap
  
-        e.preventDefault(); // double tap - prevent the zoom
-        // also synthesize click events we just swallowed up
-        $(this).trigger('click').trigger('click');
-      });
-  };
-})(jQuery);
+//         e.preventDefault(); // double tap - prevent the zoom
+//         // also synthesize click events we just swallowed up
+//         $(this).trigger('click').trigger('click');
+//       });
+//   };
+// })(jQuery);
+
+
+window.addEventListener('load', function() {
+    new FastClick(document.body);
+}, false);
